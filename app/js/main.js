e5093b4620f11d44d2fc4d88cfff80be44032c7a
triplemonster221 <boorder1993@yandex.ru>
2020-11-02 10:14:30 +0300

    added product product-slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,23 @@
 $(function(){
 
+    var mixer = mixitup('.product-inner-box');
+  
+    // $('#star').rateYo({
+    //     rating:5,
+    //     starWidth: "40px"
+
+    // });
+
+    $('.product-slider__inner').slick({
+        infinite: true,
+        arrows: false,
+        dots:true,
+        slidesToShow: 4,
+        slidesToScroll: 4
+    });
+
+  
+
+   
+
 });
--- gulpfile.js
+++ gulpfile.js
@@ -23,6 +23,7 @@ gulp.task('style', function(){
         'node_modules/normalize.css/normalize.css',
         'node_modules/slick-carousel/slick/slick.css',
         'node_modules/magnific-popup/dist/magnific-popup.css'
+        // 'node_modules/rateyo/lib/cjs/rateyo.css'
     ])
             .pipe(concat('libs.min.css'))
             .pipe(cssmin())
@@ -32,7 +33,9 @@ gulp.task('style', function(){
 gulp.task('script', function(){
     return gulp.src([
         'node_modules/slick-carousel/slick/slick.js',
-        'node_modules/magnific-popup/dist/jquery.magnific-popup.js'
+        'node_modules/magnific-popup/dist/jquery.magnific-popup.js',
+        'node_modules/mixitup/dist/mixitup.js'
+        // 'node_modules/rateyo/lib/cjs/rateyo.js'
     ])
             .pipe(concat('libs.min.js'))
             .pipe(uglify())
@@ -62,4 +65,5 @@ gulp.task('watch', function(){
     gulp.watch('app/*.html', gulp.parallel('html'));
     gulp.watch('app/js/*.js', gulp.parallel('js'));
 });
+
 gulp.task('default',gulp.parallel('style','script','sass','watch','browser-sync'))
