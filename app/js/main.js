da5adf8ab106e2ab7447eecf014916e9dd3fc88f
triplemonster221 <boorder1993@yandex.ru>
2020-11-18 07:16:32 +0300

    added setting,profile,

--- app/js/main.js
+++ app/js/main.js
@@ -1,12 +1,8 @@
 $(function(){
 
-    var mixer = mixitup('.product-inner-box');
+    
   
-    // $('#star').rateYo({
-    //     rating:5,
-    //     starWidth: "40px"
-
-    // });
+   
 
     $('.product-slider__inner').slick({
         infinite: true,
@@ -16,8 +12,69 @@ $(function(){
         slidesToScroll: 4
     });
 
-  
+    $('.top__tabs .tab').on('click', function(event) {
+      var id = $(this).attr('data-id');
+        $('.top__tabs').find('.tab-item').removeClass('active-tab').hide();
+        $('.top__tabs .tabs').find('.tab').removeClass('active');
+        $(this).addClass('active');
+        $('#'+id).addClass('active-tab').fadeIn();
+        return false;
+      });
 
+      $('.top__tabs-profile .tab-profle').on('click', function(event) {
+        var id = $(this).attr('data-id');
+          $('.top__tabs-profile').find('.tab-item-profile').removeClass('active-tab').hide();
+          $('.top__tabs-profile .tabs-profiles').find('.tab-profle').removeClass('active');
+          $(this).addClass('active');
+          $('#'+id).addClass('active-tab').fadeIn();
+          return false;
+        });
+
+  $('.header__menu-gamburger').on('click',function(){
+    $('.menu__list').slideToggle();
+    });
+
+
+  // $('#rate-star').rateYo({
+  //   rating: 5,
+  //   normalFill: "#A0A0A0", 
+  //   precision: 2
+  //   });
    
+    var mixer = mixitup('.product-inner-box');
+  
+  
+    
+ });
 
-});
+//  <div class="top__tabs">
+// // <div class="tabs">
+// //     <span class="tab active" data-id="1">Item Features</span>
+// //     <span class="tab" data-id="2">Comments </span>
+// //     <span class="tab" data-id="3">Reviews </span>
+// //     <span class="tab" data-id="4">Support</span>
+// // </div>
+// // <div class="tab_content">
+// //     <div class="tab-item active-tab tab-show" id="1">
+// //         <ul class="tab__items">
+// //             <li class="tabs__info icon-angle-right">Printing and typesetting industry</li>
+// //             <li class="tabs__info icon-angle-right">Bhen an unknown printe</li>
+// //             <li class="tabs__info icon-angle-right">Handard dummy text</li>
+// //             <li class="tabs__info icon-angle-right">Desktop publishing software</li>
+// //             <li class="tabs__info icon-angle-right">Bhen an unknown printe</li>
+// //             <li class="tabs__info icon-angle-right">Printing and typesetting industry</li>
+// //             <li class="tabs__info icon-angle-right">Bhen an unknown printe</li>
+// //             <li class="tabs__info icon-angle-right">Handard dummy text</li>
+// //         </ul>
+// //     </div>
+// //     <div class="tab-item active-tab" id="2">
+// //         2
+// //     </div>
+// //     <div class="tab-item active-tab" id="3">
+// //         3
+// //     </div>
+// //     <div class="tab-item active-tab" id="4">
+// //         4
+// //     </div>
+// // </div>
+// // </div>
--- gulpfile.js
+++ gulpfile.js
@@ -22,7 +22,8 @@ gulp.task('style', function(){
     return gulp.src([
         'node_modules/normalize.css/normalize.css',
         'node_modules/slick-carousel/slick/slick.css',
-        'node_modules/magnific-popup/dist/magnific-popup.css'
+        'node_modules/magnific-popup/dist/magnific-popup.css',
+        'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.css'
         // 'node_modules/rateyo/lib/cjs/rateyo.css'
     ])
             .pipe(concat('libs.min.css'))
@@ -34,7 +35,8 @@ gulp.task('script', function(){
     return gulp.src([
         'node_modules/slick-carousel/slick/slick.js',
         'node_modules/magnific-popup/dist/jquery.magnific-popup.js',
-        'node_modules/mixitup/dist/mixitup.js'
+        'node_modules/mixitup/dist/mixitup.js',
+        'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js'
         // 'node_modules/rateyo/lib/cjs/rateyo.js'
     ])
             .pipe(concat('libs.min.js'))
